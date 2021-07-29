84a694aa744c8c0e95d1b8410818a0d7be04df01
Weslley De Souza <we.desouza@hotmail.com>
2021-07-29 23:05:54 +0200

    Update env.ts

--- server/microservices/core/env.ts
+++ server/microservices/core/env.ts
@@ -1,7 +1,6 @@
 export const env = (()=>{
      return{
         load: (path = null)=>{
-
             path && require('fs').existsSync(path)?
                 require('dotenv').config({path: path  }) :
                  require('dotenv').config({path: (require('path').resolve(__dirname,'..','.env'))  }) ;
